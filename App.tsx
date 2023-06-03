2c618a699a05fe224e35351c0b82dea8df764479
fshafi1 <fshafi1@umbc.edu>
2023-06-03 10:41:23 -0400

    initial commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Zaddar</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    color: "white",
+    fontSize: 40,
+  },
+});
